63a65b3fd9c36163c4dbd9120514f14daa819d68
IvetaBru <ivetamarkeviciute@gmail.com>
2025-06-09 17:42:35 +0300

    basic app routes added

--- galutinis_atsiskaitymas/client/src/App.tsx
+++ galutinis_atsiskaitymas/client/src/App.tsx
@@ -1,8 +1,27 @@
+import { Routes, Route } from 'react-router';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Questions from './pages/Questions';
+import SpecificQuestion from './pages/SpecificQuestion';
+import UserInfo from './pages/UserInfo';
+import MainOutlet from './components/outlets/MainOutlet';
+
 const App = () => {
   
   return (
     <>
-    
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='' element={<MainOutlet/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/questions' element={<Questions/>}/>
+          <Route path='/questions/:_id' element={<SpecificQuestion/>}/>
+          <Route path='/user' element={<UserInfo/>}/>
+        </Route>
+      </Routes>
     </>
   )
 }
