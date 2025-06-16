54ae72752407fae3b7e5fd7453a4ae042f9b31bf
IvetaBru <ivetamarkeviciute@gmail.com>
2025-06-16 13:02:18 +0300

    answers context created

---
+++ galutinis_atsiskaitymas/client/src/contexts/AnswersContext.tsx
@@ -0,0 +1,122 @@
+import { createContext, useEffect, useReducer, useState } from "react";
+// import { useNavigate } from "react-router";
+
+import { ChildrenElementProp, Answer, AnswerActionTypes, AnswersContextType } from "../types";
+
+const reducer = (state: Answer[], action: AnswerActionTypes): Answer[] => {
+    switch(action.type){
+        case 'setData':
+            return action.data;
+        case 'addAnswer':
+            return [...state, action.newAnswer];
+        case 'deleteAnswer':
+            return state.filter(q => q._id !== action._id);
+        case 'editAnswer':
+            return state.map(answer =>
+                answer._id === action.editedAnswer._id ? action.editedAnswer : answer
+            );
+        default:
+            return state;
+    }
+}
+
+const AnswersContext = createContext<undefined | AnswersContextType>(undefined);
+const AnswersProvider = ({ children }: ChildrenElementProp) => {
+
+    const [ answers, dispatch ] = useReducer(reducer, []);
+    const [isLoading, setIsLoading] = useState(true);
+    // const navigate = useNavigate();
+
+    const addNewAnswer = async (newAnswer: Pick<Answer, 'body'>) => {
+        const accessJWT = localStorage.getItem('accessJWT') || sessionStorage.getItem('accessJWT');
+        const backResponse = await fetch(`http://localhost:5500/answers`, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                "Authorization":`Bearer ${accessJWT}`
+            },
+            body: JSON.stringify(newAnswer)
+        });
+        const data = await backResponse.json();
+        if('error' in data){
+            return { error: data.error };
+        }
+        dispatch({
+            type: "addAnswer",
+            newAnswer: data.newAnswer
+        });
+        return { success: data.success };
+    };
+
+    const deleteAnswer = (_id: Answer['_id']) => {
+        const accessJWT = localStorage.getItem('accessJWT') || sessionStorage.getItem('accessJWT');
+        const confirm = window.confirm("Do you want to delete it?");
+        if (!confirm) return;
+    
+        fetch(`http://localhost:5500/answers/${_id}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${accessJWT}`
+            },
+        })
+        .then(() => {
+            dispatch({
+                type: "deleteAnswer",
+                _id
+            });
+            // navigate('/questions')
+        });
+    }
+    const editAnswer = async (editedAnswer: Answer) => {
+        const accessJWT = localStorage.getItem('accessJWT') || sessionStorage.getItem('accessJWT');
+        const backResponse = await fetch(`http://localhost:5500/questions/${editedAnswer._id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type":"application/json",
+                Authorization: `Bearer ${accessJWT}`
+            },
+            body: JSON.stringify(editedAnswer)
+        })
+        const data = await backResponse.json();
+
+        if ('error' in data){
+            return { error: data.error };
+        }
+        dispatch({
+            type: "editAnswer",
+            editedAnswer
+        });
+        return { success: data.success };
+    }
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(`http://localhost:5500/answers`)
+            .then(res => res.json())
+            .then((data: Answer[]) => {
+                dispatch({
+                    type: "setData",
+                    data
+                });
+                setIsLoading(false);
+            });
+    }, []);    
+
+    return(
+        <AnswersContext.Provider
+            value={{
+                answers,
+                dispatch,
+                isLoading,
+                addNewAnswer,
+                deleteAnswer,
+                editAnswer
+            }}
+        >
+            { children }
+        </AnswersContext.Provider>
+    )
+}
+
+export { AnswersProvider };
+export default AnswersContext;
--- galutinis_atsiskaitymas/client/src/types.ts
+++ galutinis_atsiskaitymas/client/src/types.ts
@@ -43,33 +43,58 @@ export type QuestionActionTypes =
     { type: 'deleteQuestion', _id: Question['_id']} |
     { type: 'editQuestion', editedQuestion: Question };
     
-    export type QuestionsContextType = {
-        questions: Question[],
-        dispatch: React.ActionDispatch<[action: QuestionActionTypes]>,
-        isLoading: boolean,
-        addNewQuestion: (newQuestion: Pick<Question, 'title' | 'body' | 'tags'>) => Promise<{ error: string } | { success: string }>,
-        deleteQuestion: (_id: Question["_id"]) => void,
-        editQuestion: (editedQuestion: Question) => Promise<{ error: string } | { success: string }>
-    }
-    
-    export type InputFieldProps = {
-        inputType: string, 
-        inputName: string,
-        inputId: string,
-        inputValue: string | number,
-        inputOnChange: React.ChangeEventHandler<HTMLInputElement>,
-        inputOnBlur: React.FocusEventHandler<HTMLInputElement>,
-        labelFor: string,
-        labelText: string,
-        touched: boolean | undefined,
-        errors: string | undefined
-    };
+export type QuestionsContextType = {
+    questions: Question[],
+    dispatch: React.ActionDispatch<[action: QuestionActionTypes]>,
+    isLoading: boolean,
+    addNewQuestion: (newQuestion: Pick<Question, 'title' | 'body' | 'tags'>) => Promise<{ error: string } | { success: string }>,
+    deleteQuestion: (_id: Question["_id"]) => void,
+    editQuestion: (editedQuestion: Question) => Promise<{ error: string } | { success: string }>
+};
 
-    export type MultiSelectProps = {
-        options: string[],
-        selected: string[],
-        onChange: (selected: string[]) => void,
-        maxSelected?: number,
-        errors?: string | string[] | undefined,
-        touched?: boolean | undefined
-    }
+export type Answer = {
+    _id: string,
+    questionId: string,
+    authorId: string,
+    body: string,
+    createdAt: string,
+    updatedAt: string,
+    isEdited: boolean
+}
+
+export type AnswerActionTypes = 
+    { type: 'setData', data: Answer[] } |
+    { type: 'addAnswer', newAnswer: Answer } |
+    { type: 'deleteAnswer', _id: Answer['_id']} |
+    { type: 'editAnswer', editedAnswer: Answer };
+
+export type AnswersContextType = {
+    answers: Answer[],
+    dispatch: React.ActionDispatch<[action: AnswerActionTypes]>,
+    isLoading: boolean,
+    addNewAnswer: (newAnswer: Pick<Answer, 'body'>) => Promise<{ error: string } | { success: string }>,
+    deleteAnswer: (_id: Answer['_id']) => void,
+    editAnswer: (editedAnswer: Answer) => Promise<{ error: string } | { success: string }>
+}
+
+export type InputFieldProps = {
+    inputType: string, 
+    inputName: string,
+    inputId: string,
+    inputValue: string | number,
+    inputOnChange: React.ChangeEventHandler<HTMLInputElement>,
+    inputOnBlur: React.FocusEventHandler<HTMLInputElement>,
+    labelFor: string,
+    labelText: string,
+    touched: boolean | undefined,
+    errors: string | undefined
+};
+
+export type MultiSelectProps = {
+    options: string[],
+    selected: string[],
+    onChange: (selected: string[]) => void,
+    maxSelected?: number,
+    errors?: string | string[] | undefined,
+    touched?: boolean | undefined
+};
