cf8c0746ec95caecba5242dbc81935ce34815d22
IvetaBru <ivetamarkeviciute@gmail.com>
2025-06-18 12:56:59 +0300

    like/unlike function enabled, like button displayed in frontend

--- galutinis_atsiskaitymas/client/src/components/UI/molecules/QuestionCard.tsx
+++ galutinis_atsiskaitymas/client/src/components/UI/molecules/QuestionCard.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-import { Question } from "../../../types";
+import { Question, UserContextType } from "../../../types";
+import UsersContext from "../../../contexts/UsersContext";
 
 type Props = {
     data: Question
@@ -15,19 +18,57 @@ const StyledDiv = styled.div`
 
 const QuestionCard = ({ data }: Props) => {
 
+    const { loggedInUser } = useContext(UsersContext) as UserContextType;
     const [likesCount, setLikesCount] = useState(0);
+    const [liked, setLiked] = useState(false);
+    const accessJWT = sessionStorage.getItem("accessJWT");
 
     useEffect(() => {
     fetch(`http://localhost:5500/likes/count/${data._id}`)
         .then(res => res.json())
         .then(question => setLikesCount(question.likesCount))
         .catch(console.error);
-    },[data._id]);
+
+        if (accessJWT) {
+        fetch(`http://localhost:5500/likes/user-liked/${data._id}`, {
+        headers: {
+            Authorization:`Bearer ${accessJWT}`
+        }
+        })
+        .then(res => res.json())
+        .then(data => setLiked(data.liked))
+        .catch(console.error);
+        } 
+    },[data._id, accessJWT]);
+
+    const toggleLike = () => {
+    fetch(`http://localhost:5500/likes/toggle/${data._id}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization":`Bearer ${accessJWT}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+        setLiked(data.liked);
+        setLikesCount(prev => data.liked ? prev + 1 : prev - 1);
+        })
+        .catch(console.error);
+    };
 
     return ( 
         <StyledDiv>
             <span>{new Date(data.createdAt).toLocaleDateString()}</span>
-            <span> Likes: {likesCount}</span>
+            <div>
+                {
+                    loggedInUser ? 
+                        (<button onClick={toggleLike}>
+                            {liked ? <FavoriteIcon/> : <FavoriteBorderIcon />}
+                            {likesCount}
+                        </button>) : null
+                }
+            </div>
             <p>{data.authorUsername}</p>
             <Link to={`/questions/${data._id}`}>
                 <h3>{data.title}</h3>
--- galutinis_atsiskaitymas/server/controllers/likesController.js
+++ galutinis_atsiskaitymas/server/controllers/likesController.js
@@ -22,14 +22,50 @@ export const getAllLikes = async (req, res) => {
 export const toggleLike = async (req, res) => {
     const client = await connectDB();
     try{
+        const userId = req.user._id;
+        const { questionId } = req.params;
 
+        const likesCollection = await client
+            .db('Final_Project')
+            .collection('likes');
+
+        const existingLike = await likesCollection.findOne({ questionId, userId });
+
+        if (existingLike) {
+            await likesCollection.deleteOne({ questionId, userId });
+            return res.send({ success: 'Like removed', liked: false });
+        } else {
+            await likesCollection.insertOne({
+                _id: generateID(),  
+                questionId,
+                userId,
+                value: 1,
+            });
+        return res.send({ success: 'Liked successfully', liked: true });
+        }
     }catch(err){
-
+        console.error(err);
+        res.status(500).send({ error: err, message: `Something went wrong with server, please try again later.` });
     }finally{
         await client.close();
     }
 };
 
 export const getUserLikes = async (req, res) => {
+    const client = await connectDB();
+    try {
+        const { questionId } = req.params;
+        const userId = req.user._id;
 
+        const like = await client
+            .db('Final_Project')
+            .collection('likes')
+            .findOne({ questionId, userId });
+        res.send({ questionId, liked: !!like });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: err, message: `Something went wrong with server, please try again later.` });
+    } finally {
+        await client.close();
+    }
 };
