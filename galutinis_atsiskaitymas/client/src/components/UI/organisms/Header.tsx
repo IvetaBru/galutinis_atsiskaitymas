adb91641d9afca7ad83feb0be41f46d452eb5410
IvetaBru <ivetamarkeviciute@gmail.com>
2025-06-24 12:37:38 +0300

    header styled

--- galutinis_atsiskaitymas/client/src/components/UI/organisms/Header.tsx
+++ galutinis_atsiskaitymas/client/src/components/UI/organisms/Header.tsx
@@ -7,11 +7,77 @@ import UsersContext from '../../../contexts/UsersContext';
 import { UserContextType } from '../../../types';
 
 const StyledHeader = styled.header`
+    height: 80px;
+    padding: 0 200px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo{
+        width: 120px;
+    }
+
+    >nav{
+        >ul{
+            display: flex;
+            gap: 50px;
+            padding: 0;
+            >li{
+                list-style: none;
+                >a{
+                    text-decoration: none;
+                    font-weight: 600;
+                    color: var(--color-secondary);
+                }
+                >a.active, a:hover{
+                    color: var(--color-accentText);
+                }
+            }
+        }
+    }
+    .login{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        color: var(--color-secondary);
+        >a{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 5px;
+            text-decoration: none;
+            font-size: 15px;
+            font-weight: 600;
+        }
+        >.logout:hover{
+            cursor: pointer;
+            color: var(--color-accentText);
+        }
+    }
+    .name{
+        font-weight: 600;
+        font-style: italic;
+    }
     .photo{
-        height: 40px;
-        width: 40px;
+        height: 30px;
+        width: 30px;
         object-fit: cover;
         border-radius: 50%;
+    }
+    .questions{
+    }
+
+    @media (min-width: 0px) and (max-width: 767px) {
+        padding: 20px;
+        >nav{
+            >ul{
+                gap: 20px;
+            }
+        }
+    }
+    @media (min-width: 768px) and (max-width: 1080px) {
+        padding: 20px 100px;
     }
     
 `
@@ -24,25 +90,26 @@ const Header = () => {
     return ( 
         <StyledHeader>
             <div>
-                <h3>Logo</h3>
+                <Link to='/'><h3><img src="../../../public/images/TripTalk.svg" alt="logo" className='logo'/></h3></Link>
             </div>
             <nav>
                 <ul>
                     {
                         loggedInUser ? (
-                            <li><NavLink to='questions'>Questions</NavLink></li>
+                            <li className='questions'><NavLink to='questions'>Questions</NavLink></li>
                         ):(
                         <>
+                            <li><NavLink to='questions'>Questions</NavLink></li>
                             <li><NavLink to='/login'>Login</NavLink></li>
                             <li><NavLink to='/register'>Register</NavLink></li>   
-                            <li><NavLink to='questions'>Questions</NavLink></li>
                         </>
                         )
                     }
                 </ul>
+            </nav>    
                 {
                     loggedInUser ? (
-                        <div>
+                        <div className='login'>
                             <Link to='user'>
                                 <img 
                                     src={loggedInUser.avatar}
@@ -50,17 +117,17 @@ const Header = () => {
                                     className='photo'
                                 />
                             </Link>
-                                {loggedInUser.fullName}
+                            <span className='name'>{loggedInUser.fullName}</span>
                             <LogoutIcon 
                                 onClick={() => {
                                 logOut();
                                 navigate('/');
                             }}
+                            className='logout'
                         />
                         </div>
                     ) : null
                 }
-            </nav>    
         </StyledHeader>
      );
 }
